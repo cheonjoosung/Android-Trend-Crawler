b52854ff9fef62e95c5490650411d67249ea3426
cheonjoosung <cjswntjd@gmail.com>
2022-12-21 20:46:44 +0900

    오타수정 get->geo, ed -> ns

--- app/src/main/java/kr/co/js/trend_news/network/TrendNewsService.kt
+++ app/src/main/java/kr/co/js/trend_news/network/TrendNewsService.kt
@@ -3,27 +3,20 @@ package kr.co.js.trend_news.network
 import kr.co.js.trend_news.model.TrendResults
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface TrendNewsService {
 
+    @Headers(
+        "content-type: application/json; charset=UTF-8"
+    )
     @GET("dailytrends")
     suspend fun getGoogleTrendNews(
         @Query("hl") hl:String = "ko",
         @Query("tz") tz:String = "-540",
         @Query("ed") ed:String,
-        @Query("get") get:String = "KR",
-        @Query("ed") ns:String = "15",
+        @Query("geo") geo:String = "KR",
+        @Query("ns") ns:String = "15",
     ): Response<TrendResults>
-
-    /*@GET("search")
-    suspend fun getYouTubeVideos(
-        @Query("key") apiKey: String,
-        @Query("q") query: String,
-        @Query("order") videoOrder: String,
-        @Query("type") videoType: String = "video",
-        @Query("maxResults") maxResults: Int,
-        @Query("channelId") channelId: String = "",
-        @Query("part") part: String = "snippet",
-    ): Response<YoutubeVideo>*/
 }
